5182bfcaba6b99b253ecb31914a92beba90e2117
JinchunChoi <JinchunChoi@users.noreply.github.com>
2020-04-29 23:59:08 -0400

    Create 20-Valid_Parenthses-easy.py

---
+++ Problems/Green Problems/20-Valid_Parenthses-easy.py
@@ -0,0 +1,24 @@
+# Up: 4550, Down: 207
+
+class Solution(object):
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        
+        # Use stack, T: O(n), S: O(n)
+        stack = []
+        
+        for c in s:
+            if c == '(' or c == '{' or c == '[':
+                stack.append(c)
+            elif c == ')' or c == '}' or c == ']':
+                if stack:
+                    tmp = stack.pop()
+                else:
+                    tmp = ''
+                if (c == ')' and tmp != '(') or (c == '}' and tmp != '{') or (c == ']' and tmp != '['):
+                    return False
+                
+        return True if len(stack) == 0 else False
